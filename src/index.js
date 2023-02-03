64f7b9723ea9e1ec686b194bc63587770ebdb1b4
rishusingh022 <rishusingh022@gmail.com>
2023-02-03 12:29:24 +0530

    chore : initial setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// const express = require('express');
+
+// const  = require('./src/routes/userRoutes');
+// const app = express();
+
+// const PORT = 3000;
+
+// // app.use(express.json());
+
+// // app.use('/users', userRouter);
+
+// // Server running
+// app.listen(PORT, async () => {
+//   console.log(`Server is running at port: ${PORT}`);
+// });
