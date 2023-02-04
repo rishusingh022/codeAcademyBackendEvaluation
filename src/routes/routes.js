2fd0d75e5def6913aa723fa6d63f7e9f08d3adb9
rishusingh022 <rishusingh022@gmail.com>
2023-02-04 20:09:29 +0530

    chore : import middleware

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const controller = require('../controllers/companydetails');
-
+const {urlValidator} = require('../middlewares/validators');
 const router = require('express').Router();
 
-router.post('/save', controller.saveCompanyDetails);
+router.post('/save',urlValidator,controller.saveCompanyDetails);
 
 module.exports = router;
