901e7236a4b060c98e5c46c1bc514dcd034e66cb
rishusingh022 <rishusingh022@gmail.com>
2023-02-06 11:36:05 +0530

    fix : mock axios to test controller

--- test/controllers/companyDetails.test.js
+++ test/controllers/companyDetails.test.js
@@ -1,6 +1,6 @@
+const axios  = require('axios');
 const companyDetailsController = require('../../src/controllers/companydetails');
 const companyDetailsServices = require('../../src/services/companyDetails');
-jest.setTimeout(4000000);
 describe('TestCases for company details controller', () => {
   describe('TestCases for saveCompanyDetails', () => {
     it('Should return 201 as status code when company details are saved successfully', async () => {
@@ -13,16 +13,16 @@ describe('TestCases for company details controller', () => {
         status: jest.fn().mockReturnThis(),
         json: jest.fn()
       };
+      jest.spyOn(axios, 'get').mockResolvedValue({data:  []});
       jest.spyOn(companyDetailsServices, 'saveCompanyDetails').mockResolvedValue(1);
       jest.spyOn(companyDetailsServices, 'saveCompanySectorDetails').mockResolvedValue(1);
-      jest.spyOn(companyDetailsServices, 'updateCompanyScore').mockResolvedValue(1);
+      jest.spyOn(companyDetailsServices, 'updateCompanyScore').mockResolvedValue(1);  
       await companyDetailsController.saveCompanyDetails(mockReq, mockRes);
       expect(mockRes.status).toBeCalledWith(201);
       expect(mockRes.json).toBeCalledWith({
         message: 'Company details saved successfully'
       });
     });
-    
   });
   
 });
